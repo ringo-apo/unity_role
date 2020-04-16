c9bd91e5eaf8e4ee0ea53d3fc1cb194a082d90d1
taira_akira <taira_akira@nago_etech.local>
2020-04-16 13:20:18 +0900

    20200416

---
+++ Assets/MyObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //// 左に移動
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    this.transform.Translate(-0.1f, 0.0f, 0.0f);
+        //}
+        //// 右に移動
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    this.transform.Translate(0.1f, 0.0f, 0.0f);
+        //}
+        //// 前に移動
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    this.transform.Translate(0.0f, 0.0f, 0.1f);
+        //}
+        //// 後ろに移動
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    this.transform.Translate(0.0f, 0.0f, -0.1f);
+        //}
+    }
+}
